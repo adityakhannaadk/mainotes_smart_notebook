72b91bf86ff343529f29502f864fafce8f83ab2a
adityakhannaadk <38283382+adityakhannaadk@users.noreply.github.com>
2019-02-20 17:13:54 +0000

    Create syntactics.py

---
+++ syntactics.py
@@ -0,0 +1,27 @@
+import nltk
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+nltk.download('punkt')
+# pickle
+stemmer = nltk.stem.porter.PorterStemmer()
+remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+
+
+def stem_tokens(tokens):
+    return [stemmer.stem(item) for item in tokens]
+
+
+def normalize(text):
+    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
+
+
+vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
+
+
+def syntactic(text1, text2):
+    tfidf = vectorizer.fit_transform([text1, text2])
+    return ((tfidf * tfidf.T).A)[0, 1]
+
+
+
