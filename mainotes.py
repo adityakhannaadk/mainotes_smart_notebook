a069f00909083667134a19f920abf14e3e8e825d
adityakhannaadk <38283382+adityakhannaadk@users.noreply.github.com>
2019-07-25 14:43:42 +0100

    Update and rename syntactics.py to mainotes.py

--- mainotes.py
+++ mainotes.py
@@ -1,8 +1,9 @@
+#Search algorithm
 import nltk
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-nltk.download('punkt')
+#nltk.download('punkt')
 # pickle
 stemmer = nltk.stem.porter.PorterStemmer()
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
@@ -19,9 +20,20 @@ def normalize(text):
 vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
 
 
-def syntactic(text1, text2):
+def sim(text1, text2):
     tfidf = vectorizer.fit_transform([text1, text2])
     return ((tfidf * tfidf.T).A)[0, 1]
 
+def search(threshold,searchlist,searchquery):
+  finalist = []
+  x = 0
+  while(x < len(searchlist)):
+        if(sim(searchquery,searchlist[x])):
+          finalist.append(searchlist[x])
+        x = x + 1  
+  finalist.insert(0," Matching notes to {} \n".format(searchquery))        
+  return " ".join(finalist)
+
+#Predict and suggest algorithm (ongoing, not ready to release)
 
 
