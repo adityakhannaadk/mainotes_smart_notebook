524dddda33e7b62e2416046b47a9a10b2a1e61fd
adityakhannaadk <38283382+adityakhannaadk@users.noreply.github.com>
2019-11-05 18:05:01 +0000

    Correct PEP8 violations

--- mainotes.py
+++ mainotes.py
@@ -1,8 +1,9 @@
+from nltk.corpus import stopwords
 import nltk
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 import datetime
-#nltk.download('punkt')
+# nltk.download('punkt')
 # pickle
 stemmer = nltk.stem.porter.PorterStemmer()
 #from stemming.porter2 import stem
@@ -14,65 +15,74 @@ def stem_tokens(tokens):
 
 
 def normalize(text):
-    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
+    return stem_tokens(
+        nltk.word_tokenize(
+            text.lower().translate(remove_punctuation_map)))
 
 
 vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
 
 
 # Load library
-from nltk.corpus import stopwords
 
 # You will have to download the set of stop words the first time
-import nltk
 nltk.download('stopwords')
 
 # Load stop words
 stop_words = stopwords.words('english')
 
+
 def sim(text1, text2):
     tfidf = vectorizer.fit_transform([text1, text2])
     return ((tfidf * tfidf.T).A)[0, 1]
 
-def search_1(threshold,searchlist,searchquery):
-  finalist = []
-  x = 0
-  while(x < len(searchlist)):
-        if(sim(searchquery,searchlist[x])):
-          finalist.append(searchlist[x])
-        x = x + 1  
-  return finalist      
+
+def search_1(threshold, searchlist, searchquery):
+    finalist = []
+    x = 0
+    while(x < len(searchlist)):
+        if(sim(searchquery, searchlist[x])):
+            finalist.append(searchlist[x])
+        x = x + 1
+    return finalist
 
 
 def stem_all(sentence):
-  documents = [[stem(word) for word in sentence.split(" ")]].join*(" ")
+    documents = [[stem(word) for word in sentence.split(" ")]].join * (" ")
+
 
 def remove_stop(sentence):
-  sentence = [word for word in sentence.split(" ") if word not in stop_words].join(" ")
-  return sentence
-def append_if_not(list1,list2):
-  final = []
-  for item in list2:
-    if(item not in list1):
-      final.append(item)
-    else:
-       pass 
-  return final    
+    sentence = [word for word in sentence.split(
+        " ") if word not in stop_words].join(" ")
+    return sentence
 
-#Finding further links between 
-def clump(notes,search_t):
-  ret_docs = []
-  docs = search_1(0.4,notes,search_t)
-  for doc in docs:
-    ret_docs.extend(append_if_not(ret_docs,search_1(0.6,notes,doc)))
 
-  return ret_docs  
+def append_if_not(list1, list2):
+    final = []
+    for item in list2:
+        if(item not in list1):
+            final.append(item)
+        else:
+            pass
+    return final
 
-def search_final(threshold,searchlist,searchquery):
-  x = 0
-  finalist = clump(searchlist,searchquery)
+# Finding further links between
 
-  finalist.insert(0," Matching notes to {} \n".format(searchquery))        
-  return " ".join(finalist)
-  
-  #Search suggestions and predictions, todo for layer
+
+def clump(notes, search_t):
+    ret_docs = []
+    docs = search_1(0.4, notes, search_t)
+    for doc in docs:
+        ret_docs.extend(append_if_not(ret_docs, search_1(0.6, notes, doc)))
+
+    return ret_docs
+
+
+def search_final(threshold, searchlist, searchquery):
+    x = 0
+    finalist = clump(searchlist, searchquery)
+
+    finalist.insert(0, " Matching notes to {} \n".format(searchquery))
+    return " ".join(finalist)
+
+    # Search suggestions and predictions, todo for layer
